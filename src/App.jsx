c6c66cf3af72ce8f3148927df8a5d45481efc846
Nithin S jayan <77042006+Nithinjayan4@users.noreply.github.com>
2022-12-16 15:36:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Hero } from './Component'
+
+const App = () => {
+  return (
+    <>
+   <main>
+    <Hero/>
+   </main>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Component/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+        <div></div>
+        <div>
+            <div>
+                <h1></h1>
+                <h1></h1>
+                <button></button>
+            </div>
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
